cf4b9571626551b315904118ceac071c9fe27dee
bandwbear <bandwbear@gmail.com>
2020-06-27 12:05:49 +0300

    test

---
+++ Assets/SpawnPattern.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPattern : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int numberOfEnemies = Random.RandomRange(1, 3);
+        if (numberOfEnemies == 1) {
+        }
+    }
+
+
+    void GenFlyingEnemy(float altitude) {
+
+
+    }
+
+    void GenGroundEnemy() {
+
+
+    }
+}
